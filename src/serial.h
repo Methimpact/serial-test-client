07bf47cbd76c0b111879dd63cd30eba154052d66
methimpact <methimpact@gmail.com>
2015-08-17 16:36:27 +0200

    Update serial.h

--- src/serial.h
+++ src/serial.h
@@ -8,7 +8,7 @@
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
-int serial_init(const char *file);
+int serial_init(const char *file, const speed_t *speed);
 int serial_read(unsigned char *c);
 int serial_write(unsigned char *c);
 void serial_exit();
